bd581767831e244c144201ded78d43a0aa59efa0
mrushikh <m.rushikh@gmail.com>
2026-06-19 19:57:45 -0400

    start

---
+++ EasyOCRTest.py
@@ -0,0 +1,19 @@
+import easyocr
+import cv2
+reader=easyocr.Reader(['en'],gpu=False)
+
+image_path = r'DataCropped.jpeg'
+image = cv2.imread(image_path)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+thresh_img = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+result=reader.readtext(gray)
+tokens=[]
+number=['0','1','2','3','4','5','6','7','8','9']
+for(bbox,text,prob) in result:    
+    a= text[0]
+    for num in number:
+        if(a==num):
+            tokens.append(text)
+            break    
+
+print(tokens)  
--- videoToFrames.py
+++
@@ -1,5 +0,0 @@
-import cv2 as cv
-img = cv.imread("C:\\bookproj\BookThing\\books.jpg")
-
-cv.imshow("Display window", img)
-k = cv.waitKey(0) # Wait for a keystroke in the window
